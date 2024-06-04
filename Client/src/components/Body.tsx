a35783641bce3f87ef17c27cb68381c9ddfd645a
Shreyas M S <shreyasmadappady20@gmail.com>
2024-06-04 21:33:58 +0530

    c

--- Client/src/components/Body.tsx
+++ Client/src/components/Body.tsx
@@ -35,43 +35,40 @@ const Body: FC<BodyProps> = ({}) => {
 
   return (
     <div className="flex justify-center">
-      <div className="w-8/12 lg:w-7/12 pt-14">
-        <div className="flex flex-col items-start pb-4">
-          <div className="flex ">
-            <h1 className="pb-5 text-2xl font-semibold lg:text-4xl">
-              Chat with PDF
-            </h1>
-          </div>{" "}
-          <form className="flex flex-col items-start w-full gap-2 pb-8">
-            <label className="text-xs lg:text-sm" htmlFor="askk">
-              Upload your PDF
-            </label>
-            <input
-              className="bg-[#374151] w-full rounded-md text-xs lg:text-sm"
-              type="file"
-              name=""
-              id="askk"
-              accept="application/pdf"
-              onChange={handleChange}
-            />
-          </form>
-          {loading ? (
-            <>
-              <ContentLoader
-                speed={1}
-                backgroundColor={"#333"}
-                foregroundColor={"#999"}
-                viewBox="0 0 380 70"
-              >
-                <rect x="0" y="0" rx="4" ry="4" width="110" height="10" />
-                <rect x="0" y="15" rx="4" ry="4" width="370" height="20" />
-                <rect x="0" y="40" rx="3" ry="3" width="40" height="19" />
-              </ContentLoader>
-            </>
-          ) : (
-            uploaded && <Ask text={text} />
-          )}
-        </div>
+      <div className="w-8/12 pb-4 lg:w-7/12 pt-14">
+        <h1 className="pb-5 text-2xl font-semibold lg:text-4xl">
+          Chat with PDF
+        </h1>
+
+        <form className="flex flex-col items-start w-full gap-2 pb-8">
+          <label className="text-xs lg:text-sm" htmlFor="askk">
+            Upload your PDF
+          </label>
+          <input
+            className="bg-[#374151] w-full rounded-md text-xs lg:text-sm"
+            type="file"
+            name=""
+            id="askk"
+            accept="application/pdf"
+            onChange={handleChange}
+          />
+        </form>
+        {loading ? (
+          <>
+            <ContentLoader
+              speed={1}
+              backgroundColor={"#333"}
+              foregroundColor={"#999"}
+              viewBox="0 0 380 70"
+            >
+              <rect x="0" y="0" rx="4" ry="4" width="110" height="10" />
+              <rect x="0" y="15" rx="4" ry="4" width="370" height="20" />
+              <rect x="0" y="40" rx="3" ry="3" width="40" height="19" />
+            </ContentLoader>
+          </>
+        ) : (
+          uploaded && <Ask text={text} />
+        )}
       </div>
     </div>
   );
