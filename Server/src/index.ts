6f24fd735559610cd821908cabda435be470a6b7
Shreyas M S <shreyasmadappady20@gmail.com>
2024-06-04 23:45:54 +0530

    c

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -67,14 +67,10 @@ const Body: FC<BodyProps> = ({}) => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="#6a7280"
                   viewBox="0 0 24 24"
-                 
                   stroke="currentColor"
                   className="w-6 h-6 text-gray-500 bg-gray-500"
                 >
-                  <path
-                
-                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                  />
+                  <path d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                 </svg>
               </div>
               <div className="text-gray-600">
@@ -86,9 +82,7 @@ const Body: FC<BodyProps> = ({}) => {
                 </a>{" "}
                 or drag and drop
               </div>
-              <p className="text-sm text-gray-500">
-                PDF (max. 800x400px)
-              </p>
+              <p className="text-sm text-gray-500">PDF (max. 800x400px)</p>
             </div>
             <input
               id="example5"
