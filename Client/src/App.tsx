28ac80c93750c9b260bee545b11be18ccb49cdb1
Shreyas M S <shreyasmadappady20@gmail.com>
2024-06-05 19:40:53 +0530

    c

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,15 +1,25 @@
-import  { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import Body from "./components/Body";
 
 interface AppProps {}
 
 const App: FC<AppProps> = ({}) => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
   return (
-    <div>
-      <NavBar />
-      <Body />
-    </div>
+    <>
+      {loading ? (
+        <h1>Hello</h1>
+      ) : (
+        <div>
+          <NavBar />
+          <Body />
+        </div>
+      )}
+    </>
   );
 };
 
